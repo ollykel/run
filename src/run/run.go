2adbb34e9acb1b90fda60991fb90db0e29f386f9
OKelt <oakelton@gmail.com>
2019-04-07 22:51:00 -0700

    Revert "replaced golang with c"
    This reverts commit 88de76891f792030810fbbc319071c025f2a56c9.

---
+++ src/run/run.go
@@ -0,0 +1,18 @@
+package main
+
+/**
+ * @author Oliver Kelton, oakelton@gmail.com
+ * @date Apr 7, 2019
+ * Simple command-line utility to format printing of a process'
+ * stdout and stderr using different colors.
+ */
+
+import (
+	"os"
+	"fmt"
+)
+
+func main() {
+	
+}//-- end func main
+
